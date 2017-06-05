73d8b8b5b46a656f70270ea39a63d3317ccaf118
Adis Durakovic <adis.durakovic@gmail.com>
2017-06-05 14:42:56 +0200

    provider sezonlukdizi

---
+++ app/src/main/java/com/adisdurakovic/android/chilly/data/Stream_sezonlukdizi.java
@@ -0,0 +1,137 @@
+package com.adisdurakovic.android.chilly.data;
+
+import android.util.Log;
+
+import com.adisdurakovic.android.chilly.model.Video;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by add on 22/05/2017.
+ */
+
+public class Stream_sezonlukdizi extends StreamProvider {
+
+    String TAG="SEZONLUKDIZI";
+
+
+    @Override
+    public List<StreamSource> getStreamSources(Video video) {
+
+
+        String videotitle = video.episodeShow.title.toLowerCase().replaceAll("[^a-z0-9A-Z ]", "").replace(" ", "-");
+        String base_link = "http://sezonlukdizi.net";
+        String url = base_link + "/" + videotitle + "/" + video.seasonNumber + "-sezon-" + video.episodeNumber + "-bolum.html";
+        String video_url = base_link + "/ajax/dataEmbed.asp";
+
+        Log.d(TAG, "url: " + url);
+
+        List<StreamSource> list = new ArrayList<>();
+
+        request = new Request.Builder().url(url).build();
+
+
+        try {
+
+
+
+            Document doc = Jsoup.parse(client.newCall(request).execute().body().string());
+
+            Elements id_elems = doc.select("div.menu div[data-id]");
+
+            if(id_elems == null) return list;
+
+            for(Element id_elem : id_elems) {
+
+                String post = "id=" + id_elem.attr("data-id");
+                Log.d(TAG, "id: " + id_elem.attr("data-id"));
+
+                RequestBody body = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded; charset=UTF-8"), post);
+                String iframe_src = null;
+
+                Element iframe_elem = null;
+
+                request = new Request.Builder()
+                        .url(video_url)
+                        .addHeader("Referer", url)
+                        .post(body)
+                        .build();
+
+
+                String resp = client.newCall(request).execute().body().string();
+                iframe_elem = Jsoup.parse(resp).select("iframe").first();
+                if(iframe_elem == null) return list;
+                iframe_src = iframe_elem.attr("src");
+
+                if(!iframe_src.startsWith("http")) {
+                    iframe_src = "http:" + iframe_src;
+                }
+                Log.d(TAG, "iframe_src: " + iframe_src);
+
+                request = new Request.Builder().url(iframe_src).build();
+
+                Pattern pattern = Pattern.compile("\"?file\"?\\s*:\\s*\"([^\"]+)\"");
+                String respbody = client.newCall(request).execute().body().string();
+                Matcher matcher = pattern.matcher(respbody);
+
+
+                while(matcher.find()) {
+                    Log.d(TAG, "match: " + matcher.group());
+                    String redirect_link = matcher.group().replace("file:", "").replace("file\":", "").replace("\"", "").replace("\\", "").replace(" ", "");
+                    if(!redirect_link.startsWith("http")) {
+                        if(!redirect_link.contains("sezonlukdizi.net")) {
+                            redirect_link = base_link + redirect_link;
+                        } else {
+                            redirect_link = "http:" + redirect_link;
+                        }
+                    }
+                    Log.d(TAG, "redirect_link: " + redirect_link);
+
+                    request = new Request.Builder().url(redirect_link).build();
+                    response = client.newCall(request).execute();
+                    String src_url = response.request().url().toString();
+
+                    if(src_url.contains("google")) {
+                        StreamSource ss = new StreamSource();
+                        ss.quality = getQualityFromGoogleURL(src_url);
+                        ss.url = src_url;
+                        Log.d(TAG, "source: " + src_url);
+                        ss.provider = TAG;
+                        ss.videosource = "GVIDEO";
+                        list.add(ss);
+                    }
+
+                }
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+        return list;
+    }
+
+
+
+
+}
